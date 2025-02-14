cbfb1cfee64fa8668e108437de1ae34996ac11c4
Rushak Pachpande <rushak.pachpande06@gmail.com>
2025-02-14 20:11:46 +0530

    minor code updates

--- StudentsManagementSystem/src/main/java/com/anudip/sms/controller/StudentController.java
+++ StudentsManagementSystem/src/main/java/com/anudip/sms/controller/StudentController.java
@@ -20,46 +20,51 @@ import com.anudip.sms.model.Student;
 @RequestMapping("/student")
 public class StudentController {
 
-    private Map<Long, Student> studentEntry = new HashMap<>();
+	private Map<Long, Student> studentEntry = new HashMap<>();
 
-    // Constructor to initialize 5 default students
-    public StudentController() {
-	studentEntry.put(1L, new Student(1L, "Rushak Pachpande", 101));
-	studentEntry.put(2L, new Student(2L, "Vikas Gawande", 102));
-	studentEntry.put(3L, new Student(3L, "Avinash Bhalerao", 103));
-	studentEntry.put(4L, new Student(4L, "Shreyash Gonge", 104));
-	studentEntry.put(5L, new Student(5L, "Gaurav Bari", 105));
+	// Constructor to initialize 5 default students
+	public StudentController() {
+		studentEntry.put(1L, new Student(1L, "Rushak Pachpande", 101));
+		studentEntry.put(2L, new Student(2L, "Vikas Gawande", 102));
+		studentEntry.put(3L, new Student(3L, "Avinash Bhalerao", 103));
+		studentEntry.put(4L, new Student(4L, "Shreyash Gonge", 104));
+		studentEntry.put(5L, new Student(5L, "Gaurav Bari", 105));
+	}
 
-    }
+	@GetMapping("/check")
+	public String check() {
+		return "Site is working";
+	}
 
-    @GetMapping("/check")
-    public String check() {
-	return "Site is working";
-    }
+	@GetMapping
+	public List<Student> getAll() {
+		return new ArrayList<>(studentEntry.values());
+	}
 
-    @GetMapping
-    public List<Student> getAll() {
-	return new ArrayList<>(studentEntry.values());
-    }
+	@PostMapping
+	public boolean createEntry(@RequestBody
+	Student myEntry) {
+		studentEntry.put(myEntry.getId(), myEntry);
+		return true;
+	}
 
-    @PostMapping
-    public boolean createEntry(@RequestBody Student myEntry) {
-	studentEntry.put(myEntry.getId(), myEntry);
-	return true;
-    }
+	@GetMapping("/id/{id}")
+	public Student getStudentById(@PathVariable
+	long id) {
+		return studentEntry.get(id);
+	}
 
-    @GetMapping("/id/{id}")
-    public Student getStudentById(@PathVariable long id) {
-	return studentEntry.get(id);
-    }
+	@DeleteMapping("id/{id}")
+	public Student deleteStudentById(@PathVariable
+	long id) {
+		return studentEntry.remove(id);
+	}
 
-    @DeleteMapping("id/{id}")
-    public Student deleteStudentById(@PathVariable long id) {
-	return studentEntry.remove(id);
-    }
+	@PutMapping("id/{id}")
+	public Student updateStudentById(@PathVariable
+	long id, @RequestBody
+	Student student) {
+		return studentEntry.replace(id, student);
+	}
 
-    @PutMapping("id/{id}")
-    public Student updateStudentById(@PathVariable long id, @RequestBody Student student) {
-	return studentEntry.replace(id, student);
-    }
 }
--- StudentsManagementSystem/src/main/java/com/anudip/sms/model/Student.java
+++ StudentsManagementSystem/src/main/java/com/anudip/sms/model/Student.java
@@ -1,44 +1,18 @@
 package com.anudip.sms.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
 
-    private long id;
-    private String name;
-    private int rollNo;
-
-    public Student() {
-	super();
-    }
-
-    public Student(long id, String name, int rollNo) {
-	super();
-	this.id = id;
-	this.name = name;
-	this.rollNo = rollNo;
-    }
-
-    public long getId() {
-	return id;
-    }
-
-    public void setId(long id) {
-	this.id = id;
-    }
-
-    public String getName() {
-	return name;
-    }
-
-    public void setName(String name) {
-	this.name = name;
-    }
-
-    public int getRollNo() {
-	return rollNo;
-    }
-
-    public void setRollNo(int rollNo) {
-	this.rollNo = rollNo;
-    }
+	private long id;
+	private String name;
+	private int rollNo;
 
 }
