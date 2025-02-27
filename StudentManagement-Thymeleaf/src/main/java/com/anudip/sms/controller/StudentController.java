423f2bc1e6b8a88b5b21aed55b7b4d40e0a86bd1
Rushak Pachpande <rushak.pachpande06@gmail.com>
2025-02-27 22:41:48 +0530

    adding templates for frontend and some changes in controller for displaying Model on thymeleaf templates

--- StudentManagement-Thymeleaf/src/main/java/com/anudip/sms/controller/StudentController.java
+++ StudentManagement-Thymeleaf/src/main/java/com/anudip/sms/controller/StudentController.java
@@ -1,20 +1,20 @@
 
 package com.anudip.sms.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.anudip.sms.entity.Student;
 import com.anudip.sms.service.StudentService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/students")
@@ -23,44 +23,70 @@ public class StudentController {
 	@Autowired
 	private StudentService studentService;
 
+	// Get the List of Students page
 	@GetMapping
-	public List<Student> getAllStudents() {
-		return studentService.getAllStudents();
+	public String listStudents(Model model) {
+		model.addAttribute("students", studentService.getAllStudents());
+		return "students";
 	}
 
-	@GetMapping("/id/{id}")
-	public Student getStudentById(@PathVariable Long id) {
-		return studentService.getById(id);
+	// Get the AddStudent page
+	@GetMapping("/add")
+	public String addStudentForm(Model model) {
+		// create student object from student form data
+		Student student = new Student();
+		model.addAttribute("student", student);
+		return "add_student";
 	}
 
-	@PostMapping("/add")
-	public boolean createEntry(@RequestBody Student student) {
+	// Saving the Student
+	@PostMapping
+	public String addStudent(@ModelAttribute /* ("student") */ Student student, HttpSession session) {
 		studentService.addStudent(student);
-		return true;
+		session.setAttribute("successMessage", "Student Added");
+		return "redirect:/students";
 	}
 
-	@PutMapping("id/{id}")
-	public Student updateStudentById(@PathVariable long id, @RequestBody Student updatedStudent) {
+	// Get the Update page
+	@GetMapping("/update/{id}")
+	public String updateStudentForm(@PathVariable Long id, Model model) {
+		model.addAttribute("student", studentService.getById(id));
+		return "update_student";
+	}
 
+	// Update existing student
+	@PostMapping("/{id}")
+	public String updateStudent(@PathVariable Long id, @ModelAttribute /* ("student") */ Student student, Model model,
+			HttpSession session) {
+
+		// Get by Id
 		Student existingStudent = studentService.getById(id);
+		existingStudent.setId(id);
+		existingStudent.setFirstName(student.getFirstName());
+		existingStudent.setLastName(student.getLastName());
+		existingStudent.setRollNo(student.getRollNo());
+		existingStudent.setAddress(student.getAddress());
 
-		if (existingStudent != null) {
-			existingStudent.setFirstName(updatedStudent.getFirstName());
-			existingStudent.setLastName(updatedStudent.getLastName());
-			existingStudent.setRollNo(updatedStudent.getRollNo());
-			existingStudent.setAddress(updatedStudent.getAddress());
-
-			Student savedStudent = studentService.addStudent(existingStudent);
-			return savedStudent;
-		}
-
-		return null;
+		// Save the updated student
+		studentService.addStudent(existingStudent);
+		session.setAttribute("successMessage", "Student Updated");
+		return "redirect:/students";
 
 	}
 
-	@DeleteMapping("id/{id}")
-	public void deleteStudentById(@PathVariable Long id) {
+	// Delete student
+	@GetMapping("/{id}")
+	public String deleteStudent(@PathVariable Long id, HttpSession session) {
 		studentService.deleteById(id);
+		session.setAttribute("successMessage", "Student Deleted");
+		return "redirect:/students";
+	}
+
+	// Handler to remove popup success message as blank
+	@PostMapping("/resetSuccessMessage")
+	public ResponseEntity<Void> resetSuccessMessage(HttpSession session) {
+		session.removeAttribute("successMessage");
+		return ResponseEntity.ok().build();
 	}
 
 }
--- StudentManagement-Thymeleaf/src/main/java/com/anudip/sms/controller/StudentController1.java
+++ StudentManagement-Thymeleaf/src/main/java/com/anudip/sms/controller/StudentController1.java
@@ -1,98 +1,66 @@
 
 package com.anudip.sms.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.anudip.sms.entity.Student;
 import com.anudip.sms.service.StudentService;
 
-import jakarta.servlet.http.HttpSession;
-
-@Controller
+// @RestController
+// @RequestMapping("/students")
 public class StudentController1 {
 
+	@Autowired
 	private StudentService studentService;
 
-	public StudentController1(StudentService studentService) {
-		super();
-		this.studentService = studentService;
+	/* Postman Methods */
+
+	@GetMapping
+	public List<Student> getAllStudents() {
+		return studentService.getAllStudents();
 	}
 
-	// handler to remove popup success message as blank
-	@PostMapping("/resetSuccessMessage")
-	public ResponseEntity<Void> resetSuccessMessage(HttpSession session) {
-		session.removeAttribute("successMessage");
-		return ResponseEntity.ok().build();
+	@GetMapping("/id/{id}")
+	public Student getStudentById(@PathVariable Long id) {
+		return studentService.getById(id);
 	}
 
-	// what should get and what should not will be done here.
-
-	// handler method to handle list of students and return mode and view
-
-	@GetMapping("/students")
-	public String listStudents(Model model) {
-		model.addAttribute("students", studentService.getAllStudents());
-		return "students";
-	}
-
-	// get the add student page
-	@GetMapping("/students/new")
-	public String createStudentForm(Model model) {
-
-		// create student object from student form data
-		Student student = new Student();
-		model.addAttribute("student", student);
-		return "create_student";
-
-	}
-
-	// saving student
-	@PostMapping("/students")
-	public String saveStudent(@ModelAttribute("student") Student student, HttpSession session) {
+	@PostMapping("/add")
+	public boolean createEntry(@RequestBody Student student) {
 		studentService.addStudent(student);
-		session.setAttribute("successMessage", "Successfully Added");
-		return "redirect:/students";
+		return true;
 	}
 
-	// get the update or edit page
-	@GetMapping("/students/edit/{id}")
-	public String editStudentForm(@PathVariable Long id, Model model) {
-		model.addAttribute("student", studentService.getById(id));
-		return "edit_student";
-	}
-
-	// update data into existing table
-	@PostMapping("/students/{id}")
-	public String updateStudent(@PathVariable Long id, @ModelAttribute("student") Student student, Model model,
-			HttpSession session) {
-
-		// get student from database by id
+	//
+	@PutMapping("id/{id}")
+	public Student updateStudentById(@PathVariable long id, @RequestBody Student updatedStudent) {
 		Student existingStudent = studentService.getById(id);
-		existingStudent.setId(id);
-		existingStudent.setFirstName(student.getFirstName());
-		existingStudent.setLastName(student.getLastName());
-		existingStudent.setRollNo(student.getRollNo());
-		existingStudent.setAddress(student.getAddress());
 
-		// save update student object
-		studentService.updateStudentById(existingStudent);
-		session.setAttribute("successMessage", "Successfully Updated");
-		return "redirect:/students";
+		if (existingStudent != null) {
+			existingStudent.setFirstName(updatedStudent.getFirstName());
+			existingStudent.setLastName(updatedStudent.getLastName());
+			existingStudent.setRollNo(updatedStudent.getRollNo());
+			existingStudent.setAddress(updatedStudent.getAddress());
+			Student savedStudent = studentService.addStudent(existingStudent);
+			return savedStudent;
+		}
 
+		return null;
 	}
 
-	// handler method to delete student
-	@GetMapping("/students/{id}")
-	public String deleteStudent(@PathVariable Long id, HttpSession session) {
+	@DeleteMapping("id/{id}")
+	public void deleteStudentById(@PathVariable Long id) {
 		studentService.deleteById(id);
-		session.setAttribute("successMessage", "Successfully Deleted");
-		return "redirect:/students";
 	}
 
 }
